7deed09a3dd5f3dcd4426d5ecf020ef65c79c88c
Alyssa2026 <Alyssasun2022@gmail.com>
2024-02-23 12:05:02 -0500

    feat: login end user

---
+++ mock/src/components/LoginButton.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { Dispatch, SetStateAction } from "react";
+import { usernamePassword } from "./data/mockData";
+
+interface loginProps {
+  isLoggedIn: boolean;
+  setIsLoggedIn: Dispatch<SetStateAction<boolean>>;
+}
+
+export function LoginButton(props: loginProps) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const authenticate = () => {
+
+    if (usernamePassword.has(username)) {
+      // If the username exists, check if the password matches
+      if (usernamePassword.get(username) === password) {
+        const newValue = !props.isLoggedIn;
+        props.setIsLoggedIn(newValue);
+      } else {
+        setError("Incorrect password");
+      }
+    } else {
+      setError("Username not found");
+    }
+  };
+
+  if (props.isLoggedIn) {
+    return (
+      <button aria-label="Sign Out" onClick={authenticate}>
+        Sign out
+      </button>
+    );
+  } else {
+    return (
+      <div>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Enter Username"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter Password"
+        />
+        <button aria-label="Login" onClick={authenticate}>
+          Login
+        </button>
+        {error && <p>{error}</p>}
+      </div>
+    );
+  }
+}
---
+++ mock/src/components/data/mockData.ts
@@ -0,0 +1,5 @@
+export const usernamePassword = new Map<string, string>();
+
+// Add username-password pairs to the map
+usernamePassword.set("Alyssa", "Alyssa is Awesome");
+usernamePassword.set("Faizah", "Faizah is Awesome");
