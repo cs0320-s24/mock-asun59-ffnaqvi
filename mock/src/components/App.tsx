96723a12c28d26e8a01042e5b74bdfdd6074a423
Alyssa2026 <Alyssasun2022@gmail.com>
2024-02-23 09:15:18 -0500

    feat: basic react dependencies and outline

---
+++ mock/src/components/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/App.css';
+
+/**
+ * This is the highest level component!
+ */
+function App() {
+
+  return (
+    <div className="App">
+      <p className="App-header">
+        <h1>Mock</h1>
+      </p>
+
+    </div>
+  );
+}
+
+export default App;
