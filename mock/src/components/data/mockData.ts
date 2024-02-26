db357d9a9333161023ca4a7e38084176b3572b25
Alyssa2026 <Alyssasun2022@gmail.com>
2024-02-26 11:30:54 -0500

    feat: more mock data added

--- mock/src/components/data/mockData.ts
+++ mock/src/components/data/mockData.ts
@@ -6,25 +6,29 @@ usernamePassword.set("Faizah", "F");
 
 /**
  * A file for our mocked data containing a variety of sizes, shapes, and
- * types of mocked JSON files. 
+ * types of mocked JSON files.
  */
 const dataHeader = [
-    ["First Name", "Last Name", "State", "Phone Number", "Favorite Fruit"],
-    ["Miley", "Cyrus", "California", "123", "apple"],
-    ["Bob", "Nelson", "Rhode Island", "401401401", "pear"],
-    ["Tim", "Nelson", "Pennsylvania", "456", "apple"],
-  ];
- 
-  
-const dataNoHeader = [
-    ["Female", "Professor", "33"],
-    ["Male", "Nurse", "27"],
-    ["Female", "Lawyer", "46"],
+  ["First Name", "Last Name", "State", "Phone Number", "Favorite Fruit"],
+  ["Miley", "Cyrus", "California", "123", "apple"],
+  ["Bob", "Nelson", "Rhode Island", "401401401", "pear"],
+  ["Tim", "Nelson", "Pennsylvania", "456", "apple"],
 ];
+
+const dataNoHeader = [
+  ["Female", "Professor", "33"],
+  ["Male", "Nurse", "27"],
+  ["Female", "Lawyer", "46"],
+];
+
+const emptyData = [[]];
+
+const invalidFilePath = [["invalid File path"]];
+
+const malformedData = [["Data is malformed"]];
 // Creating file dictionary
 fileDictionary.set("header", dataHeader);
 fileDictionary.set("noHeader", dataNoHeader);
-
-  
-
- 
+fileDictionary.set("emptyData", emptyData);
+fileDictionary.set("invalidFilePath", invalidFilePath);
+fileDictionary.set("malformedData", malformedData);
