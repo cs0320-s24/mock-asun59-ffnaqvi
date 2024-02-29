60abeadb0f8ea52ff6c450896f7fed01dccb53fa
ffnaqvi <faizah_naqvi@brown.edu>
2024-02-29 12:22:28 -0500

    test: testing complete

--- mock/tests/e2e/Integration.spec.ts
+++ mock/tests/e2e/Integration.spec.ts
@@ -21,9 +21,6 @@ test("mode and load", async ({ page }) => {
     await page.getByLabel("Command input").fill("mode");
     await page.getByRole("button", { name: "Submit" }).click();
 
-    // Wait for mode switch to complete
-    await page.waitForTimeout(500); // Adjust timeout as necessary
-
     // Retrieve the mode status text
     const modeStatusText = await page.evaluate(() => {
         const modeStatusElement = document.querySelector('text[aria-label="mode status"]');
@@ -31,13 +28,10 @@ test("mode and load", async ({ page }) => {
     });
     expect(modeStatusText).toEqual(`Current mode is: verbose`);
 
-    // Execute command to load data
+    //Execute command to load data
     await page.getByLabel("Command input").click();
     await page.getByLabel("Command input").fill("load_csv emptyData");
     await page.getByRole("button", { name: "Submit" }).click();
-
-    // Wait for data loading to complete
-    await page.waitForTimeout(1000); // Adjust timeout as necessary
 
     // Check if the output is correct
     const outputText = await page.evaluate(() => {
@@ -52,4 +46,199 @@ test("mode and load", async ({ page }) => {
         return history?.children[2]?.textContent;
     });
     expect(loadStatusText).toEqual("File successfully loaded");
+});
+
+
+//tests brief load, view, search
+test("brief load, view, search", async ({ page }) => {
+    // login
+    await login(page);
+
+    //load file
+    await page.getByLabel("Command input").click();
+    await page.getByLabel("Command input").fill("load_csv header");
+    await page.getByRole("button", { name: "Submit" }).click();
+
+    //Retrieve the mode status text
+    const modeStatusText = await page.evaluate(() => {
+        const modeStatusElement = document.querySelector('text[aria-label="mode status"]');
+        return modeStatusElement?.textContent;
+    });
+    expect(modeStatusText).toEqual(`Current mode is: brief`);
+
+    // Execute command to load data
+    await page.getByLabel("Command input").click();
+    await page.getByLabel("Command input").fill("view");
+    await page.getByRole("button", { name: "Submit" }).click();
+
+    // Check if the output is correct
+    const outputText = await page.evaluate(() => {
+        const history = document.querySelector(".repl-history");
+        return history?.children[1]?.textContent;
+    });
+    expect(outputText).toEqual("FoodTypeCuisineCalories (per 100g)Price ($)SushiDishJapanese13010TacosDishMexican2186Tandoori ChickenDishIndian2208Mozerella SticksAppetizerItalian1315FalafelDishMiddle Eastern3334");
+
+    await page.getByLabel("Command input").click();
+    await page.getByLabel("Command input").fill("search 2 Indian");
+    await page.getByRole("button", { name: "Submit" }).click();
+
+    // Check if the data was successfully loaded
+    const loadStatusText = await page.evaluate(() => {
+        const history = document.querySelector(".repl-history");
+        return history?.children[2]?.textContent;
+    });
+    expect(loadStatusText).toEqual("Tandoori ChickenDishIndian2208");
+});
+
+
+//tests verbose load, view, search
+test("verbose load, view, search", async ({ page }) => {
+    // login
+    await login(page);
+
+     // Set mode to verbose
+     await page.getByLabel("Command input").click();
+     await page.getByLabel("Command input").fill("mode");
+     await page.getByRole("button", { name: "Submit" }).click();
+
+    // Retrieve the mode status text
+    const modeStatusText = await page.evaluate(() => {
+        const modeStatusElement = document.querySelector('text[aria-label="mode status"]');
+        return modeStatusElement?.textContent;
+    });
+    expect(modeStatusText).toEqual(`Current mode is: verbose`);
+
+        // Check if the output is correct
+        const modeSwitched = await page.evaluate(() => {
+            const history = document.querySelector(".repl-history");
+            return history?.children[0]?.textContent;
+        });
+        expect(modeSwitched).toEqual("Mode Switched");
+    
+        //load
+        await page.getByLabel("Command input").click();
+        await page.getByLabel("Command input").fill("load_csv header");
+        await page.getByRole("button", { name: "Submit" }).click();
+
+        const loadText = await page.evaluate(() => {
+            const history = document.querySelector(".repl-history");
+            return history?.children[1]?.textContent;
+        });
+        expect(loadText).toEqual("Command: load_csv\nOutput: ");
+    
+        const loadText2 = await page.evaluate(() => {
+            const history = document.querySelector(".repl-history");
+            return history?.children[2]?.textContent;
+        });
+        expect(loadText2).toEqual("File successfully loaded");
+
+
+    //view data
+    await page.getByLabel("Command input").click();
+    await page.getByLabel("Command input").fill("view");
+    await page.getByRole("button", { name: "Submit" }).click();
+
+    const viewText = await page.evaluate(() => {
+        const history = document.querySelector(".repl-history");
+        return history?.children[3]?.textContent;
+    });
+    expect(viewText).toEqual("Command: view\nOutput: ");
+
+    const viewText2 = await page.evaluate(() => {
+        const history = document.querySelector(".repl-history");
+        return history?.children[4]?.textContent;
+    });
+    expect(viewText2).toEqual("FoodTypeCuisineCalories (per 100g)Price ($)SushiDishJapanese13010TacosDishMexican2186Tandoori ChickenDishIndian2208Mozerella SticksAppetizerItalian1315FalafelDishMiddle Eastern3334");
+
+    await page.getByLabel("Command input").click();
+    await page.getByLabel("Command input").fill("search 2 Indian");
+    await page.getByRole("button", { name: "Submit" }).click();
+
+    //check if search successful
+    const searchText = await page.evaluate(() => {
+        const history = document.querySelector(".repl-history");
+        return history?.children[5]?.textContent;
+    });
+    expect(searchText).toEqual("Command: search\nOutput: ");
+
+    const searchText2 = await page.evaluate(() => {
+        const history = document.querySelector(".repl-history");
+        return history?.children[6]?.textContent;
+    });
+    expect(searchText2).toEqual("Tandoori ChickenDishIndian2208");
+});
+
+
+//tests brief load, view, search with invalid load
+test("invalid load, view, search", async ({ page }) => {
+    // login
+    await login(page);
+
+    //load file
+    await page.getByLabel("Command input").click();
+    await page.getByLabel("Command input").fill("load_csv malformedData");
+    await page.getByRole("button", { name: "Submit" }).click();
+
+    //view
+    await page.getByLabel("Command input").click();
+    await page.getByLabel("Command input").fill("view");
+    await page.getByRole("button", { name: "Submit" }).click();
+
+    //search
+    await page.getByLabel("Command input").click();
+    await page.getByLabel("Command input").fill("search 2 Indian");
+    await page.getByRole("button", { name: "Submit" }).click();
+
+    // Check if the output is correct
+    const outputText = await page.evaluate(() => {
+        const history = document.querySelector(".repl-history");
+        return history?.children[0]?.textContent;
+    });
+    expect(outputText).toEqual("Data is malformed");
+
+    // Check if the data was successfully loaded
+    const viewText = await page.evaluate(() => {
+        const history = document.querySelector(".repl-history");
+        return history?.children[1]?.textContent;
+    });
+    expect(viewText).toEqual("CSV file hasn't been loaded");
+
+        // Check if the data was successfully loaded
+        const searchText = await page.evaluate(() => {
+            const history = document.querySelector(".repl-history");
+            return history?.children[2]?.textContent;
+        });
+    expect(searchText).toEqual("CSV file hasn't been loaded");
+});
+
+
+//tests just search and load
+test("load and search", async ({ page }) => {
+    // login
+    await login(page);
+
+    // Set mode to verbose
+    await page.getByLabel("Command input").click();
+    await page.getByLabel("Command input").fill("load_csv header");
+    await page.getByRole("button", { name: "Submit" }).click();
+
+    //Execute command to load data
+    await page.getByLabel("Command input").click();
+    await page.getByLabel("Command input").fill("search 2 Indian");
+    await page.getByRole("button", { name: "Submit" }).click();
+
+    // Check if the data was successfully loaded
+    const loadStatusText = await page.evaluate(() => {
+        const history = document.querySelector(".repl-history");
+        return history?.children[0]?.textContent;
+    });
+    expect(loadStatusText).toEqual("File successfully loaded");
+
+        // Check if the output is correct
+        const outputText = await page.evaluate(() => {
+            const history = document.querySelector(".repl-history");
+            return history?.children[1]?.textContent;
+        });
+        expect(outputText).toEqual("Tandoori ChickenDishIndian2208");
+    
 });
--- mock/tests/e2e/Search.spec.ts
+++ mock/tests/e2e/Search.spec.ts
@@ -10,30 +10,9 @@ async function login(page) {
   await page.getByLabel("password").fill("A");
   await page.getByLabel("Login").click();
 }
-
-
-//test search without load
-test("search used without load", async ({ page }) => {
-    // login
-    login(page);
-  
-    // change to verbose mode
-    await page.getByLabel("Command input").click();
-    await page.getByLabel("Command input").fill("search 2 Indian");
-    await page.getByRole("button", { name: "Submit" }).click();
-  
-    // Values are properly stored in history after entered after successful load
-    await page.getByRole("button", { name: "Submit" }).click();
-    const secondChild = await page.evaluate(() => {
-      const history = document.querySelector(".repl-history");
-      return history?.children[0]?.textContent;
-    });
-  
-    expect(secondChild).toEqual("CSV file hasn't been loaded");
-  });
-  
+ 
 // Valid search
-test("valid search", async ({ page }) => {
+test("valid search with index", async ({ page }) => {
   // login
   login(page);
 
@@ -58,6 +37,34 @@ test("valid search", async ({ page }) => {
   });
   expect(second).toEqual("Tandoori ChickenDishIndian2208"); //search table results
 });
+
+  
+// Valid search
+test("valid search with identifier", async ({ page }) => {
+    // login
+    login(page);
+  
+    // valid load input
+    await page.getByLabel("Command input").click();
+    await page.getByLabel("Command input").fill("load_csv header");
+    await page.getByRole("button", { name: "Submit" }).click();
+  
+    const firstChild = await page.evaluate(() => {
+      const history = document.querySelector(".repl-history");
+      return history?.children[0]?.textContent;
+    });
+    expect(firstChild).toEqual("File successfully loaded"); // successful output
+  
+    await page.getByLabel("Command input").click();
+    await page.getByLabel("Command input").fill("search Cuisine Indian");
+    await page.getByRole("button", { name: "Submit" }).click();
+  
+    const second = await page.evaluate(() => {
+      const history = document.querySelector(".repl-history");
+      return history?.children[1]?.textContent;
+    });
+    expect(second).toEqual("Tandoori ChickenDishIndian2208"); //search table results
+  });
 
 // test search in verbose mode
 test("search verbose", async ({ page }) => {
@@ -172,4 +179,32 @@ test("search invalid index", async ({ page }) => {
       return history?.children[1]?.textContent;
     });
     expect(second).toEqual("Invalid Index Number");
+  });
+
+   
+// Valid search
+test("valid search with multiple output", async ({ page }) => {
+    // login
+    login(page);
+  
+    // valid load input
+    await page.getByLabel("Command input").click();
+    await page.getByLabel("Command input").fill("load_csv header");
+    await page.getByRole("button", { name: "Submit" }).click();
+  
+    const firstChild = await page.evaluate(() => {
+      const history = document.querySelector(".repl-history");
+      return history?.children[0]?.textContent;
+    });
+    expect(firstChild).toEqual("File successfully loaded"); // successful output
+  
+    await page.getByLabel("Command input").click();
+    await page.getByLabel("Command input").fill("search Type Dish");
+    await page.getByRole("button", { name: "Submit" }).click();
+  
+    const second = await page.evaluate(() => {
+      const history = document.querySelector(".repl-history");
+      return history?.children[1]?.textContent;
+    });
+    expect(second).toEqual("SushiDishJapanese13010TacosDishMexican2186Tandoori ChickenDishIndian2208FalafelDishMiddle Eastern3334"); //search table results
   });
